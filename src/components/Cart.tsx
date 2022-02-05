f325068087ba20bc0d29aa56d5712320d4539c9c
viniciusoliveiras <vinitag190@gmail.com>
2022-02-05 15:23:02 -0300

    mod 2 - alterando estado

---
+++ src/components/Cart.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Cart: React.FC = () => {
+  const state = useSelector((state) => state);
+
+  console.log(state)
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Produtos</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+
+      <tbody></tbody>
+    </table>
+  );
+};
+
+export default Cart;
