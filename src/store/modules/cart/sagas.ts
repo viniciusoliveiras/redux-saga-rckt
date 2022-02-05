3432cde1af46d588c23cdf5074a6180c13231ac0
viniciusoliveiras <vinitag190@gmail.com>
2022-02-05 16:52:03 -0300

    mod 3 - configurando redux-saga e obtendo quantidade do carrinho

---
+++ src/store/modules/cart/sagas.ts
@@ -0,0 +1,21 @@
+import { all, takeLatest, select } from "redux-saga/effects";
+import { IState } from "../..";
+import { addProductToCart } from "./actions";
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select(({ cart }: IState) => {
+    return (
+      cart.items.find((item) => item.product.id === product.id)?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity);
+
+  console.log("Adicionou ao carrinho");
+}
+
+export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
