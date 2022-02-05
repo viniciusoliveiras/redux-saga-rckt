5bab4197352525ebc2851113b8b08ad15f2130db
viniciusoliveiras <vinitag190@gmail.com>
2022-02-05 15:05:50 -0300

    mod 2 - criando servidor json

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,32 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import api from "../services/api";
+import { IProduct } from "../store/modules/cart/types";
 
 const Catalog: React.FC = () => {
-  const catalog = useSelector((state) => state);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log(catalog);
+  useEffect(() => {
+    api.get("products").then((res) => {
+      setCatalog(res.data);
+    });
+  }, []);
 
-  return <h1>Catalog</h1>;
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(({ id, price, title }) => (
+        <article key={id}>
+          <strong>{title}</strong>
+          {" - "}
+          <span>{price}</span>
+          {"  "}
+
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  );
 };
 
 export default Catalog;
